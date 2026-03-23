fb80744e1c71b59c3807f3904bc32a92724c1770
zhaoxiaoliang <zhaoxiaoliang@linkstec.com>
2026-03-23 06:58:23 +0000

    chore: clean up scratch files from debug

--- frontend/test2.ts
+++
@@ -1,8 +0,0 @@
-import { MultiTrackParser } from './src/core/stream/MultiTrackParser';
-
-const parser = new MultiTrackParser();
-parser.on('content', (data) => console.log('CONTENT:', data));
-
-parser.parse('{"track": "content", "type": "content", "delta": "I survived the rate limit storm!"}');
-parser.parse('{"track": "content", "type": "content", "content": "I survived the rate limit storm!"}');
-parser.parse('{"track": "done", "type": "done"}');
--- frontend/test_mock_parser.ts
+++
@@ -1,6 +0,0 @@
-import { MultiTrackParser } from './src/core/stream/MultiTrackParser';
-
-const parser = new MultiTrackParser();
-parser.on('content', (data) => console.log('CONTENT:', data));
-
-parser.parse('{"track": "content", "type": "con');
